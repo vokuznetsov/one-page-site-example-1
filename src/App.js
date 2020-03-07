bfc2daebd5679eb57e3728493e4901756304f7c6
Vladimir Kuznetsov <vladimir.kuznetsov@qligent.com>
2020-03-08 00:22:49 +0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './style.scss'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          "HELLO!"
+        </h1>
+
+        <form>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+            </input>
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password">
+            </input>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
       </header>
     </div>
   );
