5d5c577a96e890ffc23f208e62657b833fa731a2
Vladimir Kuznetsov <vladimir.kuznetsov@qligent.com>
2020-05-07 14:46:25 +0300

    change fonts

--- src/modules/place/Place.js
+++ src/modules/place/Place.js
@@ -9,18 +9,17 @@ class Place extends React.Component {
                 <div className="col">
                     <div className="row mt-7-vw">
                         <div className="col">
-                            <h2 className="place serif" id="lake">
-                                S. LAKE
+                            <h2 className="place float-left" id="lake">
+                               S. LAKE
                             </h2>
                         </div>
                         <div className="col d-flex flex-column" id="date">
                             <h3> JUNE 03-06, 2016</h3>
-                            <br></br>
-                            <h4>A GETAWAY FOR CREATIVES</h4>
+                            <h4 className='serif'>A GETAWAY FOR CREATIVES</h4>
                         </div>
                     </div>
 
-                    <div className="row ml-5">
+                    <div className="row">
                         <div className="col">
                             <h2 className="place float-left" id="tahoe">
                                 TAHOE
